5e51c18e33f0e46c707057f1421c748f39636417
hansungmoon <arnold1998@naver.com>
2023-07-23 03:59:55 +0900

    Fix: "monitoring ec2 terraform 수정"
    terraform의 ec2 생성 리소스의 subnet id 값 오류발생으로 인한 수정

--- terraform/dev/monitor-instance/main.tf
+++ terraform/dev/monitor-instance/main.tf
@@ -2,17 +2,20 @@ resource "aws_instance" "example_instance" {
   ami           = "ami-070374d0235f6e013"
   instance_type = "t2.micro"
   key_name      = "monitor"
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.public_sg_id]
-  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets
+  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[0]
 
 }
 
 
 ############################## data ######################################
 data "terraform_remote_state" "vpc" {
-  backend = "local"  # Replace with your actual backend configuration if using remote state
+  backend = "s3"
   config = {
-    path = "../vpc/terraform.tfstate"  # Replace with the correct path to the state file of main.tf No. 1
+    bucket = "marketboro-tf-state-bucket"
+    key    = "dev/vpc/terraform.tfstate"
+    region = "ap-northeast-2"
   }
 }
