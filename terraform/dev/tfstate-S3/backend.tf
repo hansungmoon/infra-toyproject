f6824d95c3c1ac94df573f39e2e05fcdd178a922
hansungmoon <arnold1998@naver.com>
2023-07-23 05:20:51 +0900

    Fix: "terraform tfstate-S3 수정"
    tfstate-S3의 backend.tf 수정

--- terraform/dev/tfstate-S3/backend.tf
+++ terraform/dev/tfstate-S3/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "marketboro-tf-state-bucket"
-    key    = "dev/instance/terraform.tfstate"
+    key    = "dev/tfstate-S3/terraform.tfstate"
     region = "ap-northeast-2"
   }
 }
